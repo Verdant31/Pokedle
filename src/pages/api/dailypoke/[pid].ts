a6f6d7750d315d225cfef2e8f7a83ea935c3ecdf
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-22 13:09:30 -0300

    wip

--- src/pages/api/dailypoke/[pid].ts
+++ src/pages/api/dailypoke/[pid].ts
@@ -6,6 +6,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const { pid } = req.query;
   const newPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pid}`).then(res => res.json());
   console.log(newPokemon);
-  await prisma.dailyPokemon.create({data:{name: newPokemon.name, pokemonId: newPokemon.id}})
+  const pokemonId = newPokemon.id.toString();
+  await prisma.dailyPokemon.create({data:{name: newPokemon.name, pokemonId}})
   res.status(200).json({as: 'hehe'})
 }
