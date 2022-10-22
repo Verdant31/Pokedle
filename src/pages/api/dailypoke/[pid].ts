5981ee3a169bb50669490ec6a3dfeeb9dd86e17d
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-22 13:48:06 -0300

    wip

--- src/pages/api/dailypoke/[pid].ts
+++ src/pages/api/dailypoke/[pid].ts
@@ -1,14 +1,13 @@
 // src/pages/api/examples.ts
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../server/db/client";
+import axios from 'axios';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { pid } = req.query;
   try {
-    const newPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pid}`).then(res => res.json());
-    console.log(newPokemon);
-    const pokemonId = newPokemon.id.toString();
-    await prisma.dailyPokemon.create({data:{name: newPokemon.name, pokemonId}})
+    const { name, id } = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pid}`).then((res) => res.data)
+    await prisma.dailyPokemon.create({data:{name, pokemonId: id.toString()}})
     res.status(200).json({as: 'hehe'})
   }catch(err: any) {
     console.log(err);
--- src/pages/api/updateDailyPokemon.ts
+++ src/pages/api/updateDailyPokemon.ts
@@ -1,7 +1,5 @@
 // src/pages/api/examples.ts
 import type { NextApiRequest, NextApiResponse } from "next";
-import { prisma } from "../../server/db/client";
-import * as AWS from 'aws-sdk';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("CHAMOOOOOOOOOOOOOOOOOOOOOOOOU");
