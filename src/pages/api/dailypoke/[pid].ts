d5fefa4fef3d1f3efc27fa86b41ce1e1df029019
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-22 12:02:25 -0300

    wip

--- src/pages/api/dailypoke/[pid].ts
+++ src/pages/api/dailypoke/[pid].ts
@@ -2,6 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log("CHAMOU CARALHO");
   const { pid } = req.query;
   const newPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pid}`).then(res => res.json());
   console.log(newPokemon);
