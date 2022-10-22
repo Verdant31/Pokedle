925d60efd1afb7b36c65f1e2e1f7d5f65a8b2662
Verdant31 <joao.pedro3110@hotmail.com>
2022-10-22 11:55:52 -0300

    wip

--- src/pages/api/updateDailyPokemon.ts
+++ src/pages/api/updateDailyPokemon.ts
@@ -4,6 +4,7 @@ import { prisma } from "../../server/db/client";
 import * as AWS from 'aws-sdk';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log("CHAMOOOOOOOOOOOOOOOOOOOOOOOOU");
   const { id } = req.query;
   const newPokemon = fetch(`https://pokeapi.co/api/v2/pokemon/${id}`).then(res => res.json());
   console.log(newPokemon);
